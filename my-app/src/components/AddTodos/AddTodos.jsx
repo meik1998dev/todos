5520b6078667da262614428ba0b4689f373e3bad
mohamad <m.aleik@darsoft.net>
2021-11-10 12:50:48 +0200

    crud operations on the todo list

---
+++ my-app/src/components/AddTodos/AddTodos.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import { connect } from 'react-redux';
+import { addTodos } from '../../redux/reducer.js';
+import TodoItem from '../TodoItem/TodoItem.jsx';
+
+const AddTodos = (props) => {
+   const [todo, setTodo] = useState('');
+
+   const handleChange = (e) => {
+      setTodo(e.target.value);
+   };
+
+   const add = () => {
+      if (todo === '') {
+         alert('Input is Empty');
+      } else {
+         props.addTodo({
+            id: Math.floor(Math.random() * 1000),
+            item: todo,
+            completed: false,
+         });
+         setTodo('');
+      }
+   };
+
+   return (
+      <div className='addTodos'>
+         <TextField
+            label='Your Todo'
+            variant='outlined'
+            onChange={(e) => handleChange(e)}
+            value={todo}
+         />
+
+         <Button onClick={() => add()} variant='contained'>
+            Add
+         </Button>
+         <br />
+
+         <ul>
+            {props.todos.length > 0 &&
+               props.todos.map((item) => {
+                  return <TodoItem />;
+               })}
+         </ul>
+      </div>
+   );
+};
+
+const mapStateToProps = (state) => {
+   return {
+      todos: state,
+   };
+};
+
+const mapDispatchToProps = (dispatch) => {
+   return {
+      addTodo: (obj) => dispatch(addTodos(obj)),
+   };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddTodos);
